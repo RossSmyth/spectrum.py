6ba57d53e23f40687ec05a6440f10eba867fcf5a
treefroog <crs2017@gmail.com>
2017-08-19 19:47:47 -0400

    Created: Style_range object
    This object is here to support the future, soon:tm: to be seen, block
    object. It represents the range, and style, of a style that is in a
    block. I may add an enumeration to the style attribute later.

---
+++ spectrum/style_range.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+class Style_range:
+    """Represents a Spectrum inlineStyleRange object
+
+    Supported Operations:
+
+    +-----------+-----------------------------------------+
+    | Operation |              Description                |
+    +===========+=========================================+
+    | str(x)    | Returns the style_range's style.        |
+    +-----------+-----------------------------------------+
+
+    style : str
+        The style that this style_range represents
+    offset : int
+        The offset of the style represented by the style_range from the
+        beginning of the block
+    length : int
+        The length of the style represented by the style_range starting from the
+        offset
+    """
+
+    __slots__ = [
+                'style', 'offset', 'length',
+                ]
+
+    def __init__(self, style = None, offset = None, length = None):
+        self.style = style
+        self.offset = offset
+        self.length = length
+
+    def __str__(self):
+        return self.style
